4e93ca6595b763d1fafe04028d14ce4cc1501e94
Benjamin Aronson <14benaronson@gmail.com>
2018-07-21 14:06:50 -0400

    tried a bit with contentscript

--- background.js
+++ background.js
@@ -12,3 +12,13 @@ chrome.runtime.onInstalled.addListener(function() {
         }]);
     });
 });
+chrome.runtime.onMessage.addListener(
+    function(message, callback) {
+        if(message == "runContentScript") {
+            chrome.tabs.executeScript({
+                file: 'contentscript.js'
+            });
+        }
+    });
+
+    
